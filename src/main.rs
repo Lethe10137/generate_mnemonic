773de5e9a3905ee0877be0ea64ff059dac366f34
ChenghaoLi <lichenghaongyd@qq.com>
2025-03-16 18:51:54 +0000

    feat: show eth and sol address

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,47 @@
-extern crate bip39;
-extern crate sha2;
-
-use bip39::Mnemonic;
-use sha2::{Digest, Sha256, Sha512};
 use std::env;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
 
-fn generate_mnemonic_from_entropy(entropy: &[u8]) -> Result<Vec<&'static str>, &'static str> {
+use sha2::{Digest, Sha256, Sha512};
+
+use bip39::Mnemonic;
+
+use ethers::prelude::*;
+use ethers::signers::Signer as EthSigner;
+use ethers::signers::coins_bip39::English;
+use ethers::utils::to_checksum;
+
+use solana_sdk::{
+    derivation_path::DerivationPath,
+    signer::{Signer, keypair::keypair_from_seed_and_derivation_path},
+};
+
+fn generate_mnemonic_from_entropy(
+    entropy: &[u8],
+) -> Result<(Vec<&'static str>, String, String), &'static str> {
     let mnemonic = Mnemonic::from_entropy(entropy).map_err(|_| "Invalid entropy length")?;
-    let words = mnemonic.word_iter();
-    Ok(words.collect())
+
+    //Ethereum
+    let wallet = MnemonicBuilder::<English>::default()
+        .phrase(mnemonic.to_string().as_str())
+        .build()
+        .unwrap();
+    let eth_address = to_checksum(&wallet.address(), None);
+
+    //Sol
+    let seed: [u8; 64] = mnemonic.to_seed_normalized("");
+    let derivation_path = DerivationPath::new_bip44(Some(0), Some(0));
+    let keypair = keypair_from_seed_and_derivation_path(&seed, Some(derivation_path)).unwrap();
+    let pubkey = keypair.pubkey();
+    let sol_address = pubkey.to_string();
+
+    Ok((
+        mnemonic.words().collect::<Vec<_>>(),
+        eth_address,
+        sol_address,
+    ))
 }
 
 fn hash(data: &str) -> String {
@@ -45,20 +74,22 @@ fn main() {
             let t = hash.finalize();
 
             match generate_mnemonic_from_entropy(&t[0..(mnemonic_len / 3 * 4)]) {
-                Ok(mnemonic) => {
+                Ok((mnemonic, eth, sol)) => {
                     println!("Generated Mnemonic: ");
                     let mut output = vec![];
                     mnemonic.iter().enumerate().for_each(|(i, &e)| {
-                        print!("{:02}: {:<10}", i, e);
+                        print!("{:02}: {:<10}", i + 1, e);
                         if i % 6 == 5 {
                             println!();
                         }
                         output.push(e);
                     });
-                    println!();
-                    println!();
+
+                    println!("------------------------------");
                     output.iter().for_each(|&e| print!("{} ", e));
-                    println!();
+                    println!("\n------------------------------");
+                    println!("Ethereum: {}", eth);
+                    println!("Solana: {}", sol);
                 }
                 Err(e) => eprintln!("Error: {}", e),
             };
